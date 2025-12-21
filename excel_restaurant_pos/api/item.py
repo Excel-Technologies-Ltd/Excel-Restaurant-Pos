6d4a103fa48853c977bfac31204737d2f0c57812
dev-Anamul <ah9737677@gmail.com>
2025-12-21 12:16:27 +0000

    add single items custom api

--- excel_restaurant_pos/api/item.py
+++ excel_restaurant_pos/api/item.py
@@ -42,14 +42,78 @@ def get_item_list():
     # Update form_dict with modified filters
     frappe.form_dict["filters"] = filters
 
-    print(frappe.form_dict.get("filters"))
-
     # get item list
     item_list = frappe.get_all("Item", **frappe.form_dict)
 
     # return item list
     return item_list
 	
+
+@frappe.whitelist(allow_guest=True)
+def get_item_details():
+    """
+    Get item details with variants
+    """
+
+    item_code = frappe.form_dict.get("item_code")
+    if not item_code:
+        frappe.throw("item_code is required")  
+
+    # Get item document and convert to dict
+    item_details = frappe.get_doc("Item", item_code).as_dict()
+
+    # Get variant items
+    variant_fields = ["name", "item_name", "item_code", "item_group", "image", "description"]
+    variants_items = frappe.get_all("Item", filters={"variant_of": item_code}, fields=variant_fields)
+
+    # prepare attributes
+    attributes_fields = [ "attribute","attribute_value","parent", "variant_of"]
+    attributes = frappe.get_all(
+        "Item Variant Attribute", 
+        filters={"variant_of": item_code}, 
+        fields=attributes_fields,
+        order_by="creation"
+    )
+
+    attributes_map:dict[str, list[dict]] = {}
+    for attribute in attributes:
+        if attribute.parent not in attributes_map:
+            attributes_map[attribute.parent] = []
+        attributes_map[attribute.parent].append(attribute)
+
+    # prepare addons items
+    addons_items = item_details.get("custom_addons_items", [])
+    item_codes_price = [item.item_code for item in addons_items]
+
+    
+    for variant in variants_items:
+        item_codes_price.append(variant.item_code)
+        variant.attributes = attributes_map.get(variant.item_code, [])
+
+    # item prices
+    prices = frappe.get_all(
+        "Item Price",
+        filters={
+            "item_code": ["in", item_codes_price],
+            "price_list": "Standard Selling"
+        },
+        fields=["item_code", "price_list_rate"]
+    )
+
+    price_map = {price.item_code: price.price_list_rate for price in prices}
+
+    # attach price to variants and addons   
+    for variant in variants_items:
+        variant.price = price_map.get(variant.item_code, 0)
+
+    # attach price to addons
+    for addon in addons_items:
+        addon.price = price_map.get(addon.item_code, 0)
+
+    # attach variants and addons to item details
+    item_details["variants_items"] = variants_items
+
+    return item_details
 
 @frappe.whitelist(allow_guest=True)
 def get_category_list():
