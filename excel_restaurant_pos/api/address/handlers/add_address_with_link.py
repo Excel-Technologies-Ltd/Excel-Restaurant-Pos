29cfbb1ba425593b896bf0c3ebbd9ddfdee18bbb
dev-Anamul <ah9737677@gmail.com>
2026-01-18 12:34:04 +0000

    fix: add customer address issues

--- excel_restaurant_pos/api/address/handlers/add_address_with_link.py
+++ excel_restaurant_pos/api/address/handlers/add_address_with_link.py
@@ -6,52 +6,30 @@ import frappe
 def add_address_with_link(address: dict, link_doctype: str, link_name: str) -> dict:
     """Add an address to a link."""
 
-    # Check if address with same type already exists for this link
-    address_type = address.get("address_type")
-    existing_address_name = frappe.db.get_value(
-        "Address",
+    # get the address Link First
+    address_links = frappe.get_all(
+        "Dynamic Link",
         filters={
-            "address_type": address_type,
+            "parenttype": "Address",
             "link_doctype": link_doctype,
             "link_name": link_name,
         },
-        fieldname="name",
+        fields=["parent"],
     )
 
-    # if exists, update it
-    if existing_address_name:
-        existing_address = frappe.get_doc("Address", existing_address_name)
-
-        # Update fields individually
-        for field, value in address.items():
-            if field != "links":  # Skip links, handle separately
-                setattr(existing_address, field, value)
-
-        # Update links if needed (ensure link exists)
-        link_exists = any(
-            link.link_doctype == link_doctype and link.link_name == link_name
-            for link in existing_address.links
-        )
-        if not link_exists:
-            existing_address.append(
-                "links", {"link_doctype": link_doctype, "link_name": link_name}
+    # Check if address with same type already exists for this link
+    address_type = address.get("address_type")
+    for link in address_links:
+        addr_type = frappe.db.get_value("Address", link.parent, "address_type")
+        if addr_type == address_type:
+            frappe.throw(
+                f"Address with type {address_type} already exists for this link",
+                frappe.DuplicateEntryError,
             )
 
-        # save address
-        existing_address.save(ignore_permissions=True)
-        frappe.db.commit()
-
-        return existing_address.as_dict()
-
-    # create new address
     new_address = frappe.get_doc({"doctype": "Address", **address})
-
-    # append link to address
     new_address.append("links", {"link_doctype": link_doctype, "link_name": link_name})
 
-    # insert address
     new_address.insert(ignore_permissions=True)
     frappe.db.commit()
-
-    # return address
     return new_address.as_dict()
