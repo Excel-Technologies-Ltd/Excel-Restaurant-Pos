95aeef9a8098b3f6a367f4561067874f9003882b
excel-azmin <excel.azmin@gmail.com>
2026-01-27 03:55:14 +0000

    feat: override 2fa mail template

--- excel_restaurant_pos/overrides/__init__.py
+++ excel_restaurant_pos/overrides/__init__.py
@@ -1,3 +1,8 @@
 from .item import override_item_doctype
+import frappe.twofactor
+from excel_restaurant_pos.overrides.twofactor import send_token_via_email
 
 __all__ = ["override_item_doctype"]
+
+# Override 2FA email function
+frappe.twofactor.send_token_via_email = send_token_via_email
